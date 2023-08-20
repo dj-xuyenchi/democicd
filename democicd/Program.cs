cf54cba5845b5220940099b5d02996a38d5f4323
DESKTOP-IQ2IFIG\DJ <do.quanganh99zz@gmail.com>
2023-08-20 22:40:56 +0700

    Add project files.

---
+++ democicd/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!1");
+Console.WriteLine("Hello, World!2");
+Console.WriteLine("Hello, World!3");
+Console.WriteLine("Hello, World!4");
